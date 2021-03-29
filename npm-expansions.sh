9d3436e5e66037f3e419b9a6a502b8d6f7397947
Yash Singh <saiansh2525@gmail.com>
2021-03-29 15:54:22 -0700

    Code-style

--- npm-expansions.sh
+++ npm-expansions.sh
@@ -2,39 +2,39 @@
 
 npm-expansions() {
   case $1 in
-  --help | -h)
-    echo 'npm-expansions'
-    echo
-    echo 'USAGE'
-    echo '  npm-expansions [all [--comments]|--help|-h|--name|-n|--version|-v] [-- ...]'
-    echo
-    echo '  npm-expansions                         Default: random expansion'
-    echo '  npm-expansions -- ...                  Sorts the result with ... first'
-    echo '  npm-expansions all [--comments]        Prints all of the expansions (--comments keeps comments)'
-    echo '  npm-expansions --help, -h              Print this help output'
-    echo '  npm-expansions --name, -n              Print the name'
-    echo '  npm-expansions --version, -v           Tell the version'
-    echo
-    echo 'EXAMPLES'
-    echo
-    echo '  npm-expansions'
-    echo '  npm-expansions all'
-    echo '  npm-expansions --help'
-    echo '  npm-expansions -v'
-    echo '  npm-expansions --comments'
-    echo '  npm-expansions all --comments -- '
-    echo '  npm-expansions -- sed "s/N/a/g"'
-    return
+    --help | -h)
+      echo 'npm-expansions'
+      echo
+      echo 'USAGE'
+      echo '  npm-expansions [all [--comments]|--help|-h|--name|-n|--version|-v] [-- ...]'
+      echo
+      echo '  npm-expansions                         Default: random expansion'
+      echo '  npm-expansions -- ...                  Sorts the result with ... first'
+      echo '  npm-expansions all [--comments]        Prints all of the expansions (--comments keeps comments)'
+      echo '  npm-expansions --help, -h              Print this help output'
+      echo '  npm-expansions --name, -n              Print the name'
+      echo '  npm-expansions --version, -v           Tell the version'
+      echo
+      echo 'EXAMPLES'
+      echo
+      echo '  npm-expansions'
+      echo '  npm-expansions all'
+      echo '  npm-expansions --help'
+      echo '  npm-expansions -v'
+      echo '  npm-expansions --comments'
+      echo '  npm-expansions all --comments -- '
+      echo '  npm-expansions -- sed "s/N/a/g"'
+      return
     ;;
 
-  --version | -v)
-    echo '1.0'
-    return
+    --version | -v)
+      echo '1.0'
+      return
     ;;
 
-  --name | -n)
-    echo 'npm-expansions'
-    return
+    --name | -n)
+      echo 'npm-expansions'
+      return
     ;;
 
   esac
@@ -49,20 +49,20 @@ npm-expansions() {
   FOUND_SORT=false
   for arg in "$@"; do
     case $arg in
-    --comments)
-      shift
-      NO_COMMENTS=false
+      --comments)
+        shift
+        NO_COMMENTS=false
       ;;
 
-    --)
-      shift
-      FOUND_SORT=true
-      break 2
+      --)
+        shift
+        FOUND_SORT=true
+        break 2
       ;;
 
-    *)
-      echo "Unexpected argument: \"$arg\""
-      return 1
+      *)
+        echo "Unexpected argument: \"$arg\""
+        return 1
       ;;
 
     esac
