f5017618bf817a2fe6c146725d2ccc5937d27189
Yash Singh <saiansh2525@gmail.com>
2021-03-29 15:47:20 -0700

    Update ~/.bashrc instead of .bashrc

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ else
 fi
 
 curl -s -o- https://raw.githubusercontent.com/Yash-Singh1/npm-expansions-cli/$VERSION/npm-expansions.sh > ~/npm-expansions.sh
-echo 'source ~/npm-expansions.sh' > '.bashrc'
+echo 'source ~/npm-expansions.sh' >> ~/.bashrc
 source ~/npm-expansions.sh
