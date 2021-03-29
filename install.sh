73573272783a4f9acfba84231244c9cc3654acd1
Yash Singh <saiansh2525@gmail.com>
2021-03-29 15:51:25 -0700

    Add message for loading in current shell

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ fi
 
 curl -s -o- https://raw.githubusercontent.com/Yash-Singh1/npm-expansions-cli/$VERSION/npm-expansions.sh > ~/npm-expansions.sh
 echo 'source ~/npm-expansions.sh' >> ~/.bashrc
-source ~/npm-expansions.sh
+echo 'Run source ~/npm-expansions.sh to load npm-expansions-cli in the current shell'
